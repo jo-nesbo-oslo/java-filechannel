0bab068c73bdf0bbb1e2cfc3200bd08196616b5d
jo.nesbo.oslo@gmail.com <jo.nesbo.oslo@gmail.com>
2025-02-24 17:57:45 -0600

    FileChannel example

--- src/com/solucioneskabec/ReadFileLineByLineUsingFiles.java
+++ src/com/solucioneskabec/ReadFileLineByLineUsingFiles.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class ReadFileLineByLineUsingFiles  {
+public class ReadFileLineByLineUsingFiles {
 	public static void main(String[] args) {
 		try {
 			List<String> allLines = Files.readAllLines(Paths.get("test.txt"));
