693628693df04aa418111c06832d60d19fcc5183
jo.nesbo.oslo@gmail.com <jo.nesbo.oslo@gmail.com>
2025-02-28 17:13:32 -0600

    Se agrega comentario en ruta de archivo en ReadFileInputStream

--- src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
+++ src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
@@ -9,8 +9,9 @@ public class ReadFileInputStream {
 
 	public static void main(String[] args) {
 
-		String directoryName = "resources/data_files";
-		String fileName = directoryName + "/" + "sample_1.txt";
+		//Se agrega directorio
+		String directoryName = "resources/data_files/";
+		String fileName = directoryName +  "sample_1.txt";
 
 		// Use try-with-resources to handle resource
 		// management
