61bc912607e64421022ae72d725e05c5ff8867cb
jo.nesbo.oslo@gmail.com <jo.nesbo.oslo@gmail.com>
2025-03-05 17:10:31 -0600

    Se agregan comentarios de pendientes: logger

--- src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
+++ src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
@@ -9,27 +9,34 @@ import java.io.IOException;
  */
 public class ReadFileInputStream {
 
+	/** 
+	* @author: Arturo
+	*/
 	public static void main(String[] args) {
 
 		String directoryName = "./resources/data_files/";
-		String fileName = directoryName + "sample_11.txt";
+		String fileName = directoryName + "sample_15.txt";
 
 		// Use try-with-resources to handle resource
 		// management
 		try (FileInputStream fi = new FileInputStream(fileName)) {
 
 			// Illustrating getChannel() method
+			// TODO: Cambiar por logger
 			System.out.println("Channel: " + fi.getChannel());
 
 			// Illustrating getFD() method
+			// TODO: Cambiar por logger
 			System.out.println("File Descriptor: " + fi.getFD());
 
 			// Illustrating available method
+			// TODO: Cambiar por logger
 			System.out.println("Number of remaining bytes: " + fi.available());
 
 			// Illustrating skip() method
 			fi.skip(0);
 
+			// TODO: Cambiar por logger
 			System.out.println("File Contents:");
 
 			// Reading characters from FileInputStream
@@ -38,8 +45,10 @@ public class ReadFileInputStream {
 				System.out.print((char) ch);
 			}
 		} catch (FileNotFoundException e) {
+			// TODO: Cambiar por logger
 			System.out.println("File not found: Ensure " + fileName + " exists in the working directory.");
 		} catch (IOException e) {
+			// TODO: Cambiar por logger
 			System.out.println("An error occurred while reading the file: " + e.getMessage());
 		}
 
