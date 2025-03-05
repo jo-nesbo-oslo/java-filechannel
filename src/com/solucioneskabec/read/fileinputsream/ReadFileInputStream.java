76582d434787a688bf4ee21b01510b1089613258
jo.nesbo.oslo@gmail.com <jo.nesbo.oslo@gmail.com>
2025-03-05 17:00:33 -0600

    Se cambian nombres de archivos txt en ReadFileInputStream y ReadFileLineByLineUsingFiles

--- src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
+++ src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
@@ -15,7 +15,7 @@ public class ReadFileInputStream {
 	public static void main(String[] args) {
 
 		String directoryName = "./resources/data_files/";
-		String fileName = directoryName + "sample_15.txt";
+		String fileName = directoryName + "sample_3.txt";
 
 		// Use try-with-resources to handle resource
 		// management
