caf225efc5d7c4aea8b111664ded78e21577d416
jo.nesbo.oslo@gmail.com <jo.nesbo.oslo@gmail.com>
2025-03-10 17:42:18 -0600

    Se modifican comentarios TODO

--- src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
+++ src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
@@ -14,13 +14,8 @@ public class ReadFileInputStream {
 	*/
 	public static void main(String[] args) {
 
-<<<<<<< HEAD:src/com/solucioneskabec/read/fileinputsream/ReadFileInputStream.java
 		String directoryName = "./resources/data_files/";
 		String fileName = directoryName + "sample_3.txt";
-=======
-		String directory = "./sample_directory/";
-		String fileName = directory + "sample_1.txt";
->>>>>>> 5d35d8d (Se crea directorio sample_directory y se actualizan rutas en las clases):src/com/solucioneskabec/ReadFileInputStream.java
 
 		// Use try-with-resources to handle resource
 		// management
@@ -35,13 +30,13 @@ public class ReadFileInputStream {
 			System.out.println("File Descriptor: " + fi.getFD());
 
 			// Illustrating available method
-			// TODO: Cambiar por logger
+			// TODO: Cambiar por logger Slf4j
 			System.out.println("Number of remaining bytes: " + fi.available());
 
 			// Illustrating skip() method
 			fi.skip(0);
 
-			// TODO: Cambiar por logger
+			// TODO: Cambiar por logger Slf4j
 			System.out.println("File Contents:");
 
 			// Reading characters from FileInputStream
@@ -50,10 +45,10 @@ public class ReadFileInputStream {
 				System.out.print((char) ch);
 			}
 		} catch (FileNotFoundException e) {
-			// TODO: Cambiar por logger
+			// TODO: Cambiar por logger Slf4j
 			System.out.println("File not found: Ensure " + fileName + " exists in the working directory.");
 		} catch (IOException e) {
-			// TODO: Cambiar por logger
+			// TODO: Cambiar por logger Slf4j
 			System.out.println("An error occurred while reading the file: " + e.getMessage());
 		}
 
